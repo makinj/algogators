ac2e4738b9cf267e2298c2486afadf4df97c0f9f
seveibar <seve700@gmail.com>
2015-03-20 23:53:41 -0400

    simple renderer

---
+++ app/www/js/renderer/simple/webRenderer.js
@@ -0,0 +1,73 @@
+var webRenderer = (function(){
+
+    var eApp, eCanvas;
+
+    var context;
+
+    var assetsLoaded = false;
+
+    function initialize(){
+        console.log("Initializing Renderer...");
+        eApp = $(".app")[0];
+        eCanvas = $("<canvas></canvas>")[0];
+        eCanvas.width = window.innerWidth;
+        eCanvas.height = window.innerHeight;
+        $(eApp).append(eCanvas);
+        context = eCanvas.getContext("2d");
+        initCanvas();
+        loadImages(imagesLoaded);
+    }
+
+    function imagesLoaded(){
+        assetsLoaded = true;
+        console.log("Images loaded...");
+    }
+
+    function loadImages(onFinish){
+        // TODO actually load images
+        console.log("Loading images...");
+        onFinish();
+    }
+
+    function initCanvas(){
+        clearCanvas("#fff");
+    }
+
+    function clearCanvas(color){
+        context.fillStyle = color;
+        context.fillRect(0,0,eCanvas.width, eCanvas.height);
+    }
+
+    function drawAlligator(x, y, width, height, color){
+        if (!assetsLoaded) return;
+        context.fillStyle = color;
+        context.fillRect(x,y,width,height);
+    }
+
+    function drawEgg(x,y,width,height,color){
+        if (!assetsLoaded) return;
+        context.fillStyle = color;
+        context.fillRect(x,y,width,height);
+    }
+
+    function getAlligatorSize(){
+        return {
+            width:2,
+            height:1
+        };
+    }
+    function getEggSize(){
+        return {
+            width:1,
+            height:1
+        };
+    }
+
+    return {
+        "initialize": initialize,
+        "drawEgg": drawEgg,
+        "drawAlligator": drawAlligator,
+        "getAlligatorSize": getAlligatorSize,
+        "getEggSize": getEggSize
+    };
+})();
