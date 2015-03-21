e2fd4c19e33a875fbfa71b05d5515f5c41733f0f
Eric Oswald <eoswald39@gmail.com>
2015-03-21 01:42:10 -0400

    Made function for checking if two food chains are equal. Not tested.

--- logic.js
+++ logic.js
@@ -8,6 +8,41 @@ function reduce(foodChain){
   }
 }
 
+function isEqual(foodChain1, foodChain2) {
+	if (foodChain1.length != foodChain2.length) return 0;
+	for (var i = 0; i < foodChain1.length; i++)
+		if (foodChain1[i].type != foodChain2[i].type) return 0;
+	var eggMap = {};
+	var gatorMap = {};
+	// Iterate over families and eggs (elements)
+	for (var i = 0; i < foodChain1.length; i++) {
+		var element1 = foodChain1[i];
+		var element2 = foodChain2[i];
+		if (element1.type == "egg") {
+			if (element1.colorid in eggMap && eggMap[element1.colorid] != element2.colorid)
+				return 0;
+			else if (!(element1 in eggMap))
+				eggMap[element1.colorid] = element2.colorid;
+		}
+		else if (element1.type == "family") {
+			if (element1.gators.length != element2.gators.length) return 0;
+			if (element1.foodChain.length != element2.foodChain.length) return 0;
+			// Iterate over gators
+			var gators1 = element1.gators;
+			var gators2 = element2.gators;
+			for (var i = 0; i < gators1.length(); i++) {
+				if (gators1[i].colorid in gatorMap) && gatorMap[gators1[i].colorid] != gators2[i].colorid)
+					return 0;
+				else if (!(gators1[i] in gatorMap))
+					gatorMap[gators1[i].colorid] = gators2[i].colorid;
+			}
+			// Recurse for sub-foodChains
+			if (!isEqual(element1.foodChain, element2.foodChain)) return o;
+		}
+	}
+	return 1;
+}
+
 /*
 family:
   id
