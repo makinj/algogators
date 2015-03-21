e7a898106a03fd131b5231e30ac5dccbe346c3a3
kmcnellis <kiki15@me.com>
2015-03-21 06:38:29 -0400

    Add dummy element renderer

--- app/www/js/renderer/simple/webRenderer.js
+++ app/www/js/renderer/simple/webRenderer.js
@@ -53,6 +53,22 @@ var webRenderer = (function(){
         context.closePath();
     }
 
+    function drawDummy(x,y,width,height,color){
+        //Add a placeholder function for browsers that don't have setLineDash()
+        if (!context.setLineDash) {
+            context.setLineDash = function () {}
+        }
+        if (!assetsLoaded) return;
+        context.fillStyle = color;
+        // Draw the square
+        context.beginPath();
+        context.setLineDash([5,2]);
+        context.rect(x,y,width,height);
+        context.stroke();
+        context.fill();
+        context.closePath();
+    }
+
     function getAlligatorSize(){
         return {
             width:2,
@@ -77,6 +93,7 @@ var webRenderer = (function(){
         "clear": clearCanvas,
         "drawEgg": drawEgg,
         "drawAlligator": drawAlligator,
+        "drawDummy": drawDummy,
         "getAlligatorSize": getAlligatorSize,
         "getEggSize": getEggSize,
         "getScreenSize": getScreenSize
