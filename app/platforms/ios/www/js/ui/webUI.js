8cc0106d67d4fae699d0d0904680444c068304eb
Eric Oswald <eoswald39@gmail.com>
2015-03-21 13:03:48 -0400

    Added gitignore and touch event support.

--- app/platforms/ios/www/js/ui/webUI.js
+++ app/platforms/ios/www/js/ui/webUI.js
@@ -1,11 +1,38 @@
 var webUI = (function(){
 
     function initialize(){
-        window.addEventListener("click", windowClick);
+        window.addEventListener("mousedown", windowMouseDown);
+        window.addEventListener("mouseup", windowMouseUp);
+        window.addEventListener("mousemove", windowMouseMove);
+				window.addEventListener("touchstart", windowTouchStart);
+				window.addEventListener("touchend", windowTouchEnd);
+				window.addEventListener("touchmove", windowTouchMove);
     }
 
-    function windowClick(e){
-        console.log(scene.getIdAt(e.pageX, e.pageY));
+		function windowTouchStart(e) {
+			//console.log("Touch Start");
+			scene.uiMouseDown(e.pageX, e.pageY);
+		}
+		function windowTouchEnd(e) {
+			//console.log("Touch End");
+			scene.uiMouseUp(e.pageX, e.pageY);
+		}
+		function windowTouchMove(e) {
+			//console.log("Touch Move");
+			e.preventDefault()
+			scene.uiMouseMove(e.pageX, e.pageY);
+		}
+    function windowMouseDown(e){
+			//console.log("Mouse Down");
+    	scene.uiMouseDown(e.pageX,e.pageY);
+    }
+    function windowMouseUp(e){
+			//console.log("Mouse Up");
+    	scene.uiMouseUp(e.pageX,e.pageY);
+    }
+    function windowMouseMove(e){
+			//console.log("Mouse Move");
+     	scene.uiMouseMove(e.pageX,e.pageY);
     }
 
     return {
