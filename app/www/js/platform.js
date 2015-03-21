a4ddff162db27f5ef6735fa9b9d095d01f07f5a2
seveibar <seve700@gmail.com>
2015-03-21 05:20:49 -0400

    user interface module basics

--- app/www/js/platform.js
+++ app/www/js/platform.js
@@ -5,9 +5,11 @@ var platform = (function(){
     function initialize(platformType, platformVersion){
         window.renderer = webRenderer;
         window.scene = simplerScene;
+        window.ui = webUI;
 
         renderer.initialize();
         scene.initialize();
+        ui.initialize();
     }
 
     return {
