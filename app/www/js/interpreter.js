9bed908d5de7c8fd4b81d0a78fd1a093719eec42
makinj <basicislife@gmail.com>
2015-03-22 00:03:45 +0000

    no longer changes color of every element when a family is being hatched.  This does, however make it so that it no longer beta reduces by default.  Next commit will be that

--- app/www/js/interpreter.js
+++ app/www/js/interpreter.js
@@ -437,10 +437,12 @@ var interpreter = (function(){
     newFamily = {type:"family", id:++maxId, gators:[], foodChain:[]};//create blank family with new Id
 
     for (var i=0; i < family.gators.length; i++){//add each gator over with new Id's and new color Id's
-      colorId = nextColorId();
-      colorIds[colorId]=1;
-      newFamily.gators[i]={type:"gator", id:++maxId, colorId:colorId};
-      colorMap[family.gators[i].colorId]=colorId;//mark that this colorId is changed for all children eggs
+      newFamily.gators[i]={type:"gator", id:++maxId};
+      if(family.gators[i].colorId in colorMap){
+        newFamily.gators[i].colorId=colorMap[family.gators[i].colorId];//mark that this colorId is changed for all children eggs
+      }else{
+        newFamily.gators[i].colorId=family.gators[i].colorId
+      }
     }
 
 
